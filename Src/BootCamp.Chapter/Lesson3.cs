ab8bccd55ef59d9ce74c0d9c11a1ea6d3f659510
Carter Welke <cwill365@outlook.com>
2020-01-29 12:24:33 -0800

    reupdated hw3

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            Console.WriteLine("Person 1:");
+            GetPersonData();
+            Console.WriteLine("Person 2:");
+            GetPersonData();
+
+        }
+        public static void GetPersonData()
+        {
+            string firstName = Program.GetString("Enter your First Name: ");
+
+            string lastName = Program.GetString("Enter your Last Name: ");
+
+            int age = Program.GetInt("Enter your Age: ");
+
+            float weight = Program.GetFloat("Enter your Weight(kg): ");
+
+            float height = Program.GetFloat("Enter your Height(cm): ");
+
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old,\n" +
+                "his weight is " + weight + " kg and his\n" +
+                "height is " + height + " cm.");
+
+            float bmi = Program.CalculateBmi(weight, height);
+
+            Console.WriteLine("BMI: " + bmi + " \n");
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            Lesson3.Demo();
+        }
+        public static int GetInt(string message)
+        {
+            Console.Write(message);
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static string GetString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static float GetFloat(string message)
+        {
+            Console.Write(message);
+            return (float)Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            return weight / (height * height);
         }
     }
 }
